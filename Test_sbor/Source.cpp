8d64d0cc98f9d58aa6f48bde6487a4e46b612d90
Alizbar\andr9 <st128876@student.spbu.ru>
2025-03-05 17:45:59 +0300

    Добавьте файлы проекта.

---
+++ Test_sbor/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+
+
+using namespace std;
+void main(int argc, char* argv[])
+{
+	int a = 10;
+	int* ptr = &a;
+	cout << ptr << endl;
+}
---
+++ my_first_class_on_cpp/task1.cpp
@@ -0,0 +1,198 @@
+#include<iostream>
+
+
+using namespace std;
+class ArrayList
+{ 
+private:
+	int len;
+	int* data;
+	void init(int len = 10)
+	{
+		this->len = len;
+		this->data = (int*)malloc(sizeof(int) * len);
+	}
+	bool indexValid(int index)
+	{
+		return 0 <= index < this->len;
+	}
+	void expand();
+
+public:
+	ArrayList(int len = 10)
+	{
+		this->init(len);
+		for (int i = 0; i < this->len; ++i)
+		{
+			this->data[i] = 0;
+		}
+	}
+	ArrayList(ArrayList& list)
+	{
+		//this->len = list.len;
+		//this->data = list.data;
+		this->init(list.len);
+		for (int i = 0; i < list.len; ++i)
+		{
+			this->set(i, list.get(i));
+		}
+	}
+	~ArrayList()
+	{
+		free(this->data);
+	}
+
+	void randomize(int min = 10, int max = 99)
+	{
+		for (int i = 0; i < len; ++i)
+		{
+			this->data[i] = rand() % (max - min + 1) + min;
+		}
+	}
+	void print()
+	{
+		for (int i = 0; i < this->len; ++i)
+		{
+			printf("%d ", this->data[i]);
+		}
+		printf("\n");
+	}
+	int get(int index)
+	{
+		if (indexValid(index))
+		{
+			return this->data[index];
+		}
+		return -1;
+	}
+	void set(int index, int value)
+	{
+		if (indexValid(index))
+		{
+			this->data[index] = value;
+		}
+	}
+	int count()
+	{
+		return len;
+	}
+
+	void pushBack(int element)
+	{
+		int* newData = (int*)malloc(sizeof(int) * (this->len + 1));
+
+		for (int i = 0; i < this->len; i++) 
+		{
+			newData[i] = this->data[i];
+		}
+		
+		this->len = this->len + 1;
+		this->data = (int*)malloc(sizeof(int) * (this->len));
+
+		for (int i = 0; i < this->len - 1; i++)
+		{
+			this->data[i] = newData[i];
+		}
+		this->data[this->len - 1] = element;
+	}
+
+	void pushFront(int element)
+	{
+		int* newData = (int*)malloc(sizeof(int) * (this->len + 1));
+
+		for (int i = 0; i < this->len; i++)
+		{
+			newData[i] = this->data[i];
+		}
+
+		this->len = this->len + 1;
+		this->data = (int*)malloc(sizeof(int) * (this->len));
+
+		for (int i = 1; i < this->len; i++)
+		{
+			this->data[i] = newData[i];
+		}
+		this->data[0] = element;
+	}
+
+	int popBack()
+	{
+		int* newData = (int*)malloc(sizeof(int) * (this->len));
+
+		for (int i = 0; i < this->len; i++)
+		{
+			newData[i] = this->data[i];
+		}
+
+		this->len = this->len - 1;
+		this->data = (int*)malloc(sizeof(int) * (this->len));
+
+		for (int i = 0; i < this->len; i++)
+		{
+			this->data[i] = newData[i];
+		}
+		return newData[this->len + 1];
+	}
+
+	int popFront()
+	{
+		int* newData = (int*)malloc(sizeof(int) * (this->len));
+
+		for (int i = 0; i < this->len; i++)
+		{
+			newData[i] = this->data[i];
+		}
+
+		this->len = this->len - 1;
+		this->data = (int*)malloc(sizeof(int) * (this->len));
+
+		for (int i = 0; i < this->len; i++)
+		{
+			this->data[i] = newData[i + 1];
+		}
+
+		return newData[0];
+	}
+
+	int extract(int index);
+
+	void insert(int index, int element)
+	{
+		
+	}
+
+	void reverse(int start, int end);
+
+	int sum();
+
+	int secondMax();
+
+	int lastMinIndex();
+
+	int shift(int k);
+
+	int countOdd();
+
+	int sumEven();
+};
+
+//int max(ArrayList list)
+//{
+//	int mx = list.get(0);
+//	for (int i = 0; i < list.count(); ++i)
+//	{
+//		mx = (mx > list.get(i) ? mx : list.get(i));
+//	}
+//	return mx;
+//}
+
+int main(int argc, char* argv[])
+{
+	ArrayList list(10);
+	list.randomize();
+	list.print();
+	list.popFront();
+	list.print();
+
+	return 0;
+}
